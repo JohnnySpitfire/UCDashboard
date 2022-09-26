aae17c28350e3b62a129126d4d874f5c624e2226
JohnnySpitfire <50932381+JohnnySpitfire@users.noreply.github.com>
2022-09-27 09:55:48 +1300

    git

--- powershellscripts/startUCdashboard.bat
+++ powershellscripts/startUCdashboard.bat
@@ -27,11 +27,11 @@ set "current_pid=%errorlevel%"
 for /f "skip=3 tokens=2 delims= " %%a in ('tasklist /fi "imagename eq cmd.exe"') do (
     if "%%a" neq "%current_pid%" (
         TASKKILL /PID %%a /f /T >nul 2>nul
-    )
+    )1
 )
 TIMEOUT/t 3
-start cmd /k powershell.exe -ExecutionPolicy Unrestricted -Command ". 'C:\Users\benan\Documents\GitHub\UCDashboard\powershellscripts\StartServer.ps1'"
-start cmd /k powershell.exe -ExecutionPolicy Unrestricted -Command ". 'C:\Users\benan\Documents\GitHub\UCDashboard\powershellscripts\StartFrontEnd.ps1'"
+start cmd /k powershell.exe -ExecutionPolicy Unrestricted -Command ". 'C:\Users\benan\Documents\Onedrive\GitHub\UCDashboard\powershellscripts\StartServer.ps1'"
+start cmd /k powershell.exe -ExecutionPolicy Unrestricted -Command ". 'C:\Users\benan\Documents\Onedrive\GitHub\UCDashboard\powershellscripts\StartFrontEnd.ps1'"
 echo Server Restarting ! :D
 TIMEOUT /t 1
 GOTO MENU
